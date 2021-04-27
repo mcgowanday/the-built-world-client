bc799f2227f836ec4c19a26f67c6b98de3aa53f6
mcgowanday <80540633+mcgowanday@users.noreply.github.com>
2021-04-27 03:17:42 -0400

    Add slight styling, reformatted entries

--- assets/scripts/building/ui.js
+++ assets/scripts/building/ui.js
@@ -64,7 +64,7 @@ const onViewEntriesSuccess = function (response) {
     const rating = response.buildings[i].rating
     const id = response.buildings[i]._id
     // console.log(name)
-    $('#entries-text').append(name + ', ' + city + ' –  Rating:  ' + rating + '</br>' + 'Building ID: ' + id + '</br>' + '</br>')
+    $('#entries-text').append('<u><strong>' + name + ', ' + city + '</u></strong>' + ' –  Rating:  ' + '<strong>' + rating + '</strong>' + '</br>' + '<small>Building ID: ' + id + '</small>' + '</br>' + '</br>')
     $('#view-entries').trigger('reset')
     $('#entries-text').text()
   }
